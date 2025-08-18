13a22c73d1f7aeb0f00f97758d25d6f8301fd596
alyssayuan17 <119757032+alyssayuan17@users.noreply.github.com>
2025-08-17 23:24:25 -0400

    updated urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/user/", include("user_api.urls")), # consumer app
+    path("api/provider/", include("provider_api.urls")), # servicer app
 ]
--- backend/user/main.py
+++ backend/user/main.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
-=======
 import Django
 
->>>>>>> 411030db5f939d3361316b651972535822f377ea
