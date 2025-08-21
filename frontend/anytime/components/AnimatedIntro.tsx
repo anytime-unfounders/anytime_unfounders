fb3067719256fc9a8a6b41d3ca8a33a01fb689a5
michelleJeonn <michellejeonn@gmail.com>
2025-08-21 02:56:02 -0400

    Spinning Wheels

--- frontend/anytime/components/AnimatedIntro.tsx
+++ frontend/anytime/components/AnimatedIntro.tsx
@@ -1,7 +1,46 @@
-export default function AnimatedIntro () {
+import Image from "next/image"
+export default function AnimatedIntro() {
     return (
-        <div>
-            bello
+        <div className="flex flex-col items-center justify-center p4">
+
+            <Image
+                src="/motorcyle_components/phone.svg"
+                alt="Anytime"
+                width={3000}
+                height={3000}
+                className="object-cover rounded-lg"
+            />
+            <Image
+                src="/motorcyle_components/anytime-letters.svg"
+                alt="Anytime"
+                width={3000}
+                height={3000}
+                className="absolute top-6 left-6"
+            />
+            <Image
+                src="/motorcyle_components/Rectangle.svg"
+                alt="Anytime"
+                width={150}
+                height={150}
+                className="absolute top-[700px] left-[570px] animate-spin"
+            />
+            <Image
+                src="/motorcyle_components/rightwheelcropped.svg"
+                alt="Anytime"
+                width={150}
+                height={150}
+                className="absolute top-[700px] left-[900px] animate-spin"
+            />
+
+            <Image
+                src="/motorcyle_components/new-motorcycle.svg"
+                alt="Anytime"
+                width={3000}
+                height={3000}
+                className="absolute top-6 left-6 animate-[slideIn_1s_ease-in-out]"
+            />
+
+
         </div>
     )
 }
