3d0b060001fa3d227aab34d71e980a7c0b19609b
alyssayuan17 <119757032+alyssayuan17@users.noreply.github.com>
2025-08-23 22:44:22 -0400

    created user web form

--- backend/user_api/forms.py
+++ backend/user_api/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=200)
-    your_email = forms.EmailField(label='Your email', max_length=200)
-    your_phone = forms.CharField(label='Your phone number', max_length=15)
+    user_first_name = forms.CharField(label='First Name', max_length=200)
+    user_last_name = forms.CharField(label='Last Name', max_length=200)
+    user_email = forms.EmailField(label='Email Address', max_length=200)
+    user_phone = forms.CharField(label='Phone Number', max_length=15)
+    user_address_line_1 = forms.CharField(label='Address Line #1', max_length=300)
+    user_address_line_2 = forms.CharField(label='Address Line #2', max_length=300, required=False)
+    user_city = forms.CharField(label='City', max_length=100)
+    user_postal_code = forms.CharField(label='Postal Code', max_length=20)
+    user_province_state = forms.CharField(label='Province/State', max_length=100)
+    user_country = forms.CharField(label='Country', max_length=100)
--- backend/user_api/views.py
+++ backend/user_api/views.py
@@ -10,13 +10,22 @@ def get_name(request):
         form = NameForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            # process the data in form.cleaned_data (variables can now be used-- save to database, send email, etc.)
+            user_first_name = form.cleaned_data['user_first_name']
+            user_last_name = form.cleaned_data['user_last_name']
+            user_email = form.cleaned_data['user_email']
+            user_phone = form.cleaned_data['user_phone']
+            user_address_line_1 = form.cleaned_data['user_address_line_1']
+            user_address_line_2 = form.cleaned_data['user_address_line_2']
+            user_city = form.cleaned_data['user_city']
+            user_postal_code = form.cleaned_data['user_postal_code']
+            user_province_state = form.cleaned_data['user_province_state']
+            user_country = form.cleaned_data['user_country']
+            # redirect to a page after successfully submitting form:
+            return HttpResponseRedirect('/thanks/') 
 
-    # if a GET (or any other method) we'll create a blank form
+    # if any other method, use NameForm to generate form, validate, access cleaned data
     else:
         form = NameForm()
 
-    return render(request, 'name.html', {'form': form})
+    return render(request, 'name.html', {'form': form}) # return form available as a variable named 'form'
