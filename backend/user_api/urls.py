a3d4772f38325da010dcd95ae7e6b8cdc5f46f7e
alyssayuan17 <119757032+alyssayuan17@users.noreply.github.com>
2025-08-24 01:23:02 -0400

    update urls.py

--- backend/user_api/urls.py
+++ backend/user_api/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
     # password reset tools
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
@@ -13,12 +11,3 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
-'''from django.urls import path
-from .views import prepare_checkout, resume_checkout, create_setupintent
-
-urlpatterns = [
-    path("checkout/prepare/", prepare_checkout),
-    path("checkout/resume/", resume_checkout),
-    path("payment/setup-intent/", create_setupintent),
-]
-'''
