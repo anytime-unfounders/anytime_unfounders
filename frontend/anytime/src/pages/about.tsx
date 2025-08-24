3db6857fca8f41285371b385c236faf43fcec13b
Amogh <gajelliamogh@gmail.com>
2025-08-24 12:24:47 -0400

    multiple pages

---
+++ frontend/anytime/src/pages/about.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import AnimatedIntro from "../../components/AnimatedIntro/AnimatedIntro";
+
+export default function about() {
+  return <div> about us bello</div>;
+}
