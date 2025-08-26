d3d6022b8f958c638ff92721ba815696b7e10781
alyssayuan17 <119757032+alyssayuan17@users.noreply.github.com>
2025-08-26 15:26:41 -0400

    register custom provider model for admin

--- backend/provider_api/admin.py
+++ backend/provider_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Provider
 
-# Register your models here.
+admin.site.register(Provider) 
+# register provider model to be visible + manageable on django admin site
