2c3c9844a3bc601e4052466c3aa9a24014950d58
Matthew Li <limatthew68@gmail.com>
2025-08-30 23:58:54 -0400

    afejoi

--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import get_user_model
 
-User=settings.AUTH_USER_MODEL
+User = get_user_model()
+
+#User=settings.AUTH_USER_MODEL
 
 class UserProfile(models.Model): # create profile model to extend user information
     user = models.OneToOneField(User, on_delete=models.CASCADE)
