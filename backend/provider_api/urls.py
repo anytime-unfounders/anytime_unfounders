f06d98c5939903b6ae68e2bfbeb2e456d41f233b
alyssayuan17 <119757032+alyssayuan17@users.noreply.github.com>
2025-09-05 18:21:43 -0400

    wtf

--- backend/provider_api/urls.py
+++ backend/provider_api/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from .forms import AccountInfoForm, BankingInfoForm, ServiceInfoForm, ProfilePhotosForm, ProviderRegistrationForm
 from . import views
 from django.contrib.auth import views as auth_views
-afjewaiofjwaeoi
 
 urlpatterns = [
     path('register/', views.register, name='register'),
